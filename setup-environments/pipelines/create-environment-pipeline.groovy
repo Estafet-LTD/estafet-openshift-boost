77b578c9c50e004a13e6894a560b3deb9b203ea8
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-01-05 23:06:03 +0000

    updated framework for the console

--- setup-environments/pipelines/create-environment-pipeline.groovy
+++ setup-environments/pipelines/create-environment-pipeline.groovy
@@ -41,8 +41,6 @@ node {
 	     string(name: 'ADMIN_PASSWORD'),
 	     string(name: 'PRODUCT'),
 	     string(name: 'USER'),
-	     string(name: 'UPDATE'),
-	     string(name: 'TEST'),
 	  ])
 	])
 	
@@ -54,35 +52,17 @@ node {
 		sh "oc login --insecure-skip-tls-verify=true -u ${params.ADMIN_USER} -p ${params.ADMIN_PASSWORD} ${params.MASTER_HOST}"
 	}
 	
-	if (params.UPDATE.equals("false")) {
-		stage ("create the namespace") {
-			project = getNextProjectName()
-			def title = params.PROJECT_TITLE.equals("") ? project : params.PROJECT_TITLE
-			sh "oc new-project $project --display-name='${title}'"
-			sh "oc label namespace $project type=dq product=${params.PRODUCT}"
-			sh "oc policy add-role-to-user edit system:serviceaccount:${params.PRODUCT}-cicd:jenkins -n $project"
-			sh "oc policy add-role-to-user edit ${params.USER} -n $project"
-		}		
-	}
-	
-	stage ("create the message broker") {
-		sh "oc process amq63-basic -n openshift -p IMAGE_STREAM_NAMESPACE=openshift -p MQ_USERNAME=amq -p MQ_PASSWORD=amq | oc create -f -"
-		sh "oc set probe dc/broker-amq --readiness --remove"
-		openshiftVerifyDeployment namespace: project, depCfg: "broker-amq", replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000" 
-	}
-	
-	stage ("create the jaeger server") {
-		sh "oc process -f https://raw.githubusercontent.com/jaegertracing/jaeger-openshift/master/all-in-one/jaeger-all-in-one-template.yml | oc create -f -"
-	}
+	stage ("create the namespace") {
+		project = getNextProjectName()
+		def title = params.PROJECT_TITLE.equals("") ? project : params.PROJECT_TITLE
+		sh "oc new-project $project --display-name='${title}'"
+		sh "oc label namespace $project type=dq product=${params.PRODUCT}"
+		sh "oc policy add-role-to-user edit system:serviceaccount:${params.PRODUCT}-cicd:jenkins -n $project"
+		sh "oc policy add-role-to-user edit ${params.USER} -n $project"
+	}		
 	
 	stage ("create the database endpoint") {
 		sh "oc export svc postgresql -n ${params.PRODUCT}-build --as-template=postgresql | oc apply -f -"
 	}
-	
-	def microservices = readYaml file: "setup-environments/vars/microservices-vars.yml"
-	microservices[0].each { microservice ->
-		stage ('create each microservice') {
-		}
-	}		
 	
 }
