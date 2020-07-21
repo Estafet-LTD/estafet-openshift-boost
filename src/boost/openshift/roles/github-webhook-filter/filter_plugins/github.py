61fdbaa974e5e74b588aa0a822776686a1f0d58c
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-07-21 09:23:33 +0100

    update the python filter implementation

--- src/boost/openshift/roles/github-webhook-filter/filter_plugins/github.py
+++ src/boost/openshift/roles/github-webhook-filter/filter_plugins/github.py
@@ -2,7 +2,10 @@
 import re
 
 class FilterModule(object):
-    def github_webhook_repo(self):
+    def filters(self):
+        return self.github_webhook_repo
+
+    def github_webhook_repo(self, var):
         regex = r"https:\/\/github.com\/([a-zA-Z0-9-_]+)\/([a-zA-Z0-9-_]+)(\.git)?"
-        matches = re.findall(regex, self)
+        matches = re.findall(regex, var)
         return >>> matches[1] + '/' + matches[2]
