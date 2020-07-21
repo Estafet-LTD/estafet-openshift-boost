eef53a806fd09d7a055f355b9017bfac26f02b67
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-07-21 11:04:46 +0100

    changed to github_repo filter

--- src/boost/openshift/roles/github-webhook-teardown/filter_plugins/github.py
+++ src/boost/openshift/roles/github-webhook-teardown/filter_plugins/github.py
@@ -4,10 +4,10 @@ import re
 class FilterModule(object):
     def filters(self):
         return {
-            'to_github_repo' : self.to_github_repo
+            'github_repo' : self.github_repo
         }
 
-    def to_github_repo(self, var):
+    def github_repo(self, var):
         regex = r"https:\/\/github.com\/([a-zA-Z0-9-_]+)\/([a-zA-Z0-9-_]+)(\.git)?"
         matches = re.findall(regex, var)
         repo = matches[1] + '/' + matches[2]
