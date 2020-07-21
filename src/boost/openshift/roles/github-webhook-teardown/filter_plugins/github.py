b77b2f9ff4f057153bf7ef6a37dcb05f35371663
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-07-21 11:22:13 +0100

    fixed the composite grouping

--- src/boost/openshift/roles/github-webhook-teardown/filter_plugins/github.py
+++ src/boost/openshift/roles/github-webhook-teardown/filter_plugins/github.py
@@ -10,6 +10,4 @@ class FilterModule(object):
     def github_repo(self, var):
         regex = r"https:\/\/github.com\/([a-zA-Z0-9-_]+)\/([a-zA-Z0-9-_]+)(\.git)?"
         match = re.search(regex, var)
-        print("Match at index %s, %s" % (match.start(), match.end()))
-        repo = match.group(1) + '/' + match.group(1)
-        return repo
+        return match.group(1) + '/' + match.group(2)
