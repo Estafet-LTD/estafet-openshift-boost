54620fa411817ccdd42f2dfc8310277d97fd5543
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-08-20 11:14:05 +0100

    fixed the repo url filter

--- src/boost/openshift/roles/_init/filter_plugins/get_repo_url.py
+++ src/boost/openshift/roles/_init/filter_plugins/get_repo_url.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import ConfigParser
+import re
 
 class FilterModule(object):
     def filters(self):
@@ -8,6 +8,13 @@ class FilterModule(object):
         }
 
     def get_repo_url(self, path):
-        config = ConfigParser.ConfigParser()
-        config.read(path)
-        return config['remote "origin"']['url']
+        with open(path) as fp:
+            line = fp.readline()
+            while line:
+                regex = r"(url)(\s*\=\s*)(https:\/\/github.com\/[a-zA-Z0-9-_]+\/[a-zA-Z0-9-_]+(\.git)?)"
+                if re.search(regex, path):
+                    match = re.search(regex, path)
+                    return match.group(3)
+                else:
+                    line = fp.readline()
+        print("invalid config: %s" % path)
