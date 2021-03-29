72e680a226ce981b95d5a1c8200be6c376d331cc
Dennis Williams <dennyjoewilliams@yahoo.com>
2021-03-29 21:30:16 +0100

    fixed metadata name lookup[

---
+++ src/boost/openshift/roles/_init/filter_plugins/metadata_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import json
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'metadata_name' : self.metadata_name
+        }
+
+    def metadata_name(self, stdout):
+        dcs = json.loads(stdout)
+        items = dcs["items"]
+        if not items:
+            return ""
+        else:
+            return items[0]["spec"]["metadata"]["name"]
