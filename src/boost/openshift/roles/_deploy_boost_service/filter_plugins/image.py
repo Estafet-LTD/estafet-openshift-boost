cc4ba4cbc2c7474fc51b32d3901f65e3452122d8
Dennis Williams <dennyjoewilliams@yahoo.com>
2021-03-29 20:54:20 +0100

    updated image retrieval

---
+++ src/boost/openshift/roles/_deploy_boost_service/filter_plugins/image.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import json
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'image' : self.image
+        }
+
+    def image(self, json):
+        dcs = json.loads(json)
+        items = dcs["items"]
+        if not items:
+            return ""
+        else:
+            return items[0]["spec"]["template"]["spec"]["containers"][0]["image"]
