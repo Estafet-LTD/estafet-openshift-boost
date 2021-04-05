3cb604d2b5a0e3be44b18c2ce4afeac8ba9457fd
Dennis Williams <dennyjoewilliams@yahoo.com>
2021-04-05 19:39:36 +0100

    calculate the next stage automatically

---
+++ src/boost/openshift/roles/_update_environments/filter_plugins/next_stage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import json
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'next_stage' : self.next_stage
+        }
+
+    def next_stage(self, stage, stages):
+        if stage == "build":
+            return "next"
+        elif stage == "prod":
+            return "end"        
+        for i in range(len(stages)):      
+            if stage == stages[i]["name"]:
+                return stages[i+1]["name"]
