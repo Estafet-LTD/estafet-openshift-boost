fd7e7e1e1ce01a2fac24af6595a92c84747b47b6
Dennis Williams <dennyjoewilliams@yahoo.com>
2021-04-06 16:16:46 +0100

    update match count

--- src/boost/openshift/roles/_init/filter_plugins/match_count.py
+++ src/boost/openshift/roles/_init/filter_plugins/match_count.py
@@ -10,11 +10,8 @@ class FilterModule(object):
     def match_count(self, stdout, match):
         pods = json.loads(stdout)
         items = pods["items"]
-        if not items:
-            return 0
-        else:
-            count = 0
-            for item in items:
-                if item["metadata"]["name"].startswith(match):
-                    count++
-            return count
+        count = 0
+        for item in items:
+            if item["metadata"]["name"].startswith(match):
+                count++
+        return count
